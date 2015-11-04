8381c8da7a4d2e1a58ff41afc9208d2d500062b5
Jan Renz <jan.renz@gmail.com>
2015-11-04 22:41:46 +0100

    handle update of values

---
+++ app/components/count-upp.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['ember-countup'],
+  tagName: 'span',
+  countUp: null,
+  didInsertElement() {
+    this._insertAndStartCountUp();
+  },
+
+  changed: Ember.observer('startVal', 'decimals', 'duration', 'useEasing', 'useGrouping', 'separator', 'decimal', 'prefix', 'suffix', function() {
+    this._insertAndStartCountUp();
+  }),
+  changed_value: Ember.observer('endVal', function() {
+    this._update();
+  }),
+  _update(){
+    this.get('countUp').update(this.get('endVal'));
+  },
+  _insertAndStartCountUp() {
+    Ember.run.next(() => {
+      this.set('countUp', new CountUp(
+        this.get('elementId'),
+        this.get('startVal') || 0,
+        this.get('endVal') || 0,
+        this.get('decimals') || 0,
+        this.get('duration') || 2,
+        {
+          useEasing: this.get('useEasing'),
+          useGrouping: this.get('useGrouping'),
+          separator: this.get('separator') || ',',
+          decimal: this.get('decimal') || '.',
+          prefix: this.get('prefix') || '',
+          suffix: this.get('suffix') || ''
+        }
+      ));
+      this.get('countUp').start();
+    });
+  }
+});
