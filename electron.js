5518e6a383ebe56fd646c1dc3625b7984d160f7e
Jan Renz <jan.renz@gmail.com>
2015-11-04 23:43:47 +0100

    toggle fullscreen

--- electron.js
+++ electron.js
@@ -2,6 +2,7 @@ var BrowserWindow = require('browser-window');
 var app = require('app');
 var mainWindow = null;
 var menubar = require('menubar');
+var ipc = require('ipc');
 
 
 if (process.env.ELECTRON_ENV === 'development') {
@@ -13,7 +14,16 @@ if (process.env.ELECTRON_ENV === 'development') {
 var mb = menubar({'index': url})
 mb.on('ready', function ready (){
   console.log('app is ready');
+  // In main process.
 })
+ipc.on('synchronous-message', function(event, arg) {
+  //console.log(mb.window.isFullScreen());
+  if (arg == 'fullscreen'){
+    mb.window.setFullScreen(!mb.window.isFullScreen());
+  }
+});
+
+
 /**app.on('window-all-closed', function onWindowAllClosed() {
     if (process.platform !== 'darwin') {
         app.quit();
